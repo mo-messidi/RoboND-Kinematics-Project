d9f8545659e92cee2f8e538f644e37a95b03a975
mo-messidi <mo@messidi.com>
2018-01-29 08:53:23 -0800

    numerical instabilites handled

--- kuka_arm/scripts/IK_server.py
+++ kuka_arm/scripts/IK_server.py
@@ -28,31 +28,30 @@ d1, d2, d3, d4, d5, d6, d7 = symbols('d1:8')
 a0, a1, a2, a3, a4, a5, a6 = symbols('a0:7')
 alpha0, alpha1, alpha2, alpha3, alpha4, alpha5, alpha6 = symbols('alpha0:7')
 
+# Create Modified DH parameters
+dh_params = {
+    alpha0:     0,      a0: 0,      d1: 0.75,   q1: q1,
+    alpha1:     -pi/2., a1: 0.35,   d2: 0,      q2: q2-pi/2.,
+    alpha2:     0,      a2: 1.25,   d3: 0,      q3: q3,
+    alpha3:     -pi/2., a3: -0.054, d4: 1.5,    q4: q4,
+    alpha4:     pi/2.,  a4: 0,      d5: 0,      q5: q5,
+    alpha5:     -pi/2., a5: 0,      d6: 0,      q6: q6,
+    alpha6:     0,      a6: 0,      d7: 0.303,  q7: 0
+    }
+
+# Define Modified DH Transformation matrix
+def TF_Matrix(alpha, a, d, q):
+    tf_matrix = Matrix([[cos(q), -sin(q), 0, a],
+                [sin(q) * cos(alpha), cos(q) * cos(alpha), -sin(alpha), -sin(alpha) * d],
+                [sin(q) * sin(alpha), cos(q) * sin(alpha), cos(alpha), cos(alpha) * d],
+                [0, 0, 0, 1]])
+    return tf_matrix
+
 def handle_calculate_IK(req):
     rospy.loginfo("Received %s eef-poses from the plan" % len(req.poses))
     if len(req.poses) < 1:
         print "No valid poses received"
         return -1
-    else:
-    
-        # Create Modified DH parameters
-        dh_params = {
-            alpha0:     0,      a0: 0,      d1: 0.75,   q1: q1,
-            alpha1:     -pi/2., a1: 0.35,   d2: 0,      q2: q2-pi/2.,
-            alpha2:     0,      a2: 1.25,   d3: 0,      q3: q3,
-            alpha3:     -pi/2., a3: -0.054, d4: 1.5,    q4: q4,
-            alpha4:     pi/2.,  a4: 0,      d5: 0,      q5: q5,
-            alpha5:     -pi/2., a5: 0,      d6: 0,      q6: q6,
-            alpha6:     0,      a6: 0,      d7: 0.303,  q7: 0
-            }
-    #
-    # Define Modified DH Transformation matrix
-    def TF_Matrix(alpha, a, d, q):
-        tf_matrix = Matrix([[cos(q), -sin(q), 0, a],
-                   [sin(q) * cos(alpha), cos(q) * cos(alpha), -sin(alpha), -sin(alpha) * d],
-                   [sin(q) * sin(alpha), cos(q) * sin(alpha), cos(alpha), cos(alpha) * d],
-                   [0, 0, 0, 1]])
-        return tf_matrix
     
     # Create individual transformation matrices
     T0_1 = TF_Matrix(a0,alpha0,d1,q1).subs(dh_params)
@@ -130,16 +129,23 @@ def handle_calculate_IK(req):
         R0_3 = T0_1[0:3,0:3] * T1_2[0:3,0:3] * T2_3[0:3,0:3]
         R0_3 = R0_3.evalf(subs={q1: theta1, q2: theta2, q3: theta3})
 
-        R3_6 = R0_3.inv("LU") * ROT_GRIP
+        #R3_6 = R0_3.inv("LU") * ROT_GRIP <-- Changed from inverse to transpose to reduce numerical instabilites
+        R3_6 = R0_3.T * ROT_GRIP
 
-        #Euler angles from rotation matrix
+        #Euler angles from rotation matrix taking mutliple solutions into account
         theta4 = atan2(R3_6[2,2], -R3_6[0,2])
         theta5 = atan2(sqrt(R3_6[0,2]*R3_6[0,2] + R3_6[2,2]*R3_6[2,2]),R3_6[1,2])
-        theta6 = atan2(-R3_6[1,1], R3_6[1,0])
+
+      	#Multiple solutions handling to ensure consistency
+      	if sin(theta5) < 0:
+        	theta4 = atan2(-R3_6[2,2], R3_6[0,2])
+        	theta6 = atan2(R3_6[1,1], -R3_6[1,0])
+    	else:
+        	theta4 = atan2(R3_6[2,2], -R3_6[0,2])
+        	theta6 = atan2(-R3_6[1,1], R3_6[1,0])
 
 
-            # Populate response for the IK request
-            # In the next line replace theta1,theta2...,theta6 by your joint angle variables
+        # Populate response for the IK request
         joint_trajectory_point.positions = [theta1, theta2, theta3, theta4, theta5, theta6]
         joint_trajectory_list.append(joint_trajectory_point)
 
